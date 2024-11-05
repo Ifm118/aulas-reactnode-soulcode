3675c3017f7f57d601a13973b82f062a9090c95b
ifm118 <ifroes.dev@gmail.com>
2024-11-05 09:22:52 -0300

    aula 10

---
+++ aula 10/atividade.js
@@ -0,0 +1,40 @@
+// 1. Considerando as variáveos x = 7 e y = 8, crie um código que mostre:
+// A soma dos números.
+// A diferença entre os números.
+// A multiplicação dos números.
+// A divisão dos números.
+// Dica: Utilize os operadores +, -, * e /.
+
+letx = 7, y=8;
+console.log("x = ", x, "y =", y);
+console.log("Soma:",x+y);
+console.log("Subtração: ", x - y);
+console.log("Multiplicação", x * y);
+console.log("DivisãoÇ ", x / y);
+console.log("Exponenciação: ", x ** y);
+
+
+
+// 2. Dado o valor let x = 10, utilize operadores de atribuição para modificar e exibir o valor de x da seguinte forma:
+// Adicione 5 a x.
+// Subtraia 3 de x.
+// Multiplique x por 2.
+// Divida x por 4.
+// Dica: Utilize operadores como +=, -=, *=, e /=.
+
+x=10;
+x = x + 5;
+console.log("X = ", x);
+
+
+// 3. Escreva um código que tenha duas strings e verifica:
+// Se as duas strings são iguais (utilizando ==).
+// Se as duas strings são diferentes (utilizando !=).
+// 4. Escreva um código que tenha dois numeros e verifica:
+// Se o primeiro numero é maior que o segundo.
+// Se o primeiro numero é menor ou igual ao segundo.
+// 5. Crie um código que tenha três valores booleanos e mostre:
+// A conjunção de todos eles.
+// A disjunção de todos eles.
+// A negação da disjunção de todos eles.
+// Utilize os operadores lógicos &&, || e !
